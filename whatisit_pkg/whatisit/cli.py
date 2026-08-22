f22080ad956c1ae234d8fafa68f616f4a423bd30
BlueNihilist <vasiliy.petrom@gmail.com>
2026-08-22 14:43:27 +0300

    [chore] double check OS on --prefill-command

--- whatisit_pkg/whatisit/cli.py
+++ whatisit_pkg/whatisit/cli.py
@@ -270,8 +270,10 @@ def cmd_query(args, cfg: dict) -> int:
         return 6
 
     if args.prefill_command or cfg.get("prefill_command", False):
-        # Windows machines have returned from cmd_query() by now and we can
-        # use `readline` which Python includes on Unix systems by default.
+        if _is_windows():
+            print("whatisit: -p/--prefill-command is not supported on Windows", file=sys.stderr)
+            return 9
+
         import readline
 
         readline.set_startup_hook(lambda: readline.insert_text(chosen))
